d46ce4b9b0c8b95f38a1fe0553088787a0c9e092
ezequielarraygada <137341946+ezequielarraygadareact@users.noreply.github.com>
2023-06-26 13:05:04 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Navbar from "./assets/components/layout/Navbar/Navbar";
+import Home from "./assets/components/pages/home/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
