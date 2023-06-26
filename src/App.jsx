dbada7c6e5b978ff2828e5f79c2706436df294a4
ezequielarraygada <137341946+ezequielarraygadareact@users.noreply.github.com>
2023-06-26 13:16:24 -0300

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./assets/components/layout/Navbar/Navbar";
 import Home from "./assets/components/pages/home/Home";
+import Cart from "./assets/components/pages/cart/Cart";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <Cart />
     </div>
   );
 }
---
+++ src/assets/components/pages/cart/Cart.jsx
@@ -0,0 +1,11 @@
+import styles from "./Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div className={styles.contenedor}>
+      <h1>Carrito</h1>
+    </div>
+  );
+};
+
+export default Cart;
