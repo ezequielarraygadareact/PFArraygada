9a8bc0cd80353dbbac661076a08fc2e10de26441
ezequielarraygadareact <137341946+ezequielarraygadareact@users.noreply.github.com>
2023-08-18 23:54:55 -0300

    firebase

--- src/components/pages/itemList/ItemListContainer.jsx
+++ src/components/pages/itemList/ItemListContainer.jsx
@@ -2,25 +2,29 @@ import "./ItemListContainer.css";
 import ItemList from "./ItemList";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { products } from "../../../productsMocks";
+import { dat } from "../../../firebaseconfig";
+import { getDocs, collection, query, where } from "firebase/firestore"
+
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
-  const [error, setError] = useState({});
 
   const { categoryName } = useParams();
 
   useEffect(() => {
-    let productsFiltrados = products.filter(
-      (elemento) => elemento.category === categoryName
-    );
-    const tarea = new Promise((resolve, reject) => {
-      resolve(categoryName === undefined ? products : productsFiltrados);
-    });
 
-    tarea
-      .then((respuesta) => setItems(respuesta))
-      .catch((error) => setError(error));
+    let productsCollection = collection(dat, "products")
+    let categoryCollection = query(productsCollection, where("category", "==", categoryName))
+    
+    let consulta
+    if(categoryName){
+      consulta = categoryCollection
+    }else{
+      consulta = productsCollection
+    }
+    getDocs(consulta)
+
+    
   }, [categoryName]);
 
   return <ItemList items={items} />;
