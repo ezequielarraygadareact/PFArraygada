916f7f478ad4d4073ce699de0589a7e9ecf97269
ezequielarraygadareact <137341946+ezequielarraygadareact@users.noreply.github.com>
2023-08-29 09:59:58 -0300

    fix

--- src/components/pages/itemList/ItemListContainer.jsx
+++ src/components/pages/itemList/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import "./ItemListContainer.css";
 import ItemList from "./ItemList";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { dat } from "../../../firebaseconfig.js";
+import { dat } from "../../../firebaseConfig";
 import { getDocs, collection, query, where } from "firebase/firestore"
 
 
